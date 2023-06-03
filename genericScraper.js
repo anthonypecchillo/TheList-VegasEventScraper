b23e6debcda7f5b295e0677668c13848f31a0343
Anthony Pecchillo <anthonypecchillo@gmail.com>
2023-06-02 18:31:09 -0700

    Implement simple GenericScraper class

---
+++ genericScraper.js
@@ -0,0 +1,32 @@
+const puppeteer = require('puppeteer');
+
+class GenericScraper {
+  constructor() {
+    this.browser = null;
+    this.pages = {};
+  }
+
+  async init() {
+    this.browser = await puppeteer.launch({ headless: true });
+  }
+
+  async close() {
+    if (this.browser) {
+      await this.browser.close();
+    }
+  }
+
+  async scrapeWebsite(websiteModule, url) {
+    let page = this.pages[url];
+
+    if (!page) {
+      page = await this.browser.newPage();
+      this.pages[url] = page;
+      await page.goto(url, { waitUntil: 'networkidle2' });
+    }
+
+    return await websiteModule.scrape(page);
+  }
+}
+
+module.exports = GenericScraper;
