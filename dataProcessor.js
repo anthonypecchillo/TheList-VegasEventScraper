32a56628cbbfc3cfbb7d6697bb3e90a09593aa82
Anthony Pecchillo <anthonypecchillo@gmail.com>
2023-06-02 19:18:37 -0700

    Init DataProcessor class

---
+++ dataProcessor.js
@@ -0,0 +1,21 @@
+class DataProcessor {
+  constructor() { }
+
+  cleanData(data) {
+    // Implement cleaning logic here.
+  }
+
+  standardizeDate(date) {
+    // Implement date standardizing logic here.
+  }
+
+  categorizeEvent(event) {
+    // Implement categorization logic here.
+  }
+
+  process(data) {
+    // Overall data processing logic.
+  }
+}
+
+module.exports = DataProcessor;
