1734009f8a766abceb9f0ed7843b618c2e0dc7f3
Anthony Pecchillo <anthonypecchillo@gmail.com>
2023-06-02 19:08:48 -0700

    Add a helper function for extracting text content from the page

--- specificWebsiteScraper.js
+++ specificWebsiteScraper.js
@@ -12,6 +12,13 @@ class SpecificWebsiteScraper extends GenericScraper {
   async scrape(page) {
     // Custom logic to scrape the event data.
   }
+
+  async extractInnerText(page, selector) {
+    return page.evaluate((selector) => {
+      const element = document.querySelector(selector);
+      return element ? element.innerText : null;
+    }, selector);
+  }
 }
 
 module.exports = SpecificWebsiteScraper;
