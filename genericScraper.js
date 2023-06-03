d3cde160cec00f97b273c1a5161b2df5c9754648
Anthony Pecchillo <anthonypecchillo@gmail.com>
2023-06-02 18:49:10 -0700

    Add respect for robots.txt

--- genericScraper.js
+++ genericScraper.js
@@ -1,13 +1,18 @@
 const puppeteer = require('puppeteer');
 const UserAgent = require('user-agents');
+const RobotsParser = require('robots-parser');
+const axios = require('axios');
 
 class GenericScraper {
   constructor() {
+    // Initialize the browser and pages variables
     this.browser = null;
     this.pages = {};
+    this.robotsTxtCache = {};
   }
 
   async init() {
+    // Launch a new browser instance with a random user agent
     this.browser = await puppeteer.launch({
       headless: true,
       userAgent: new UserAgent().toString(),
@@ -15,25 +20,51 @@ class GenericScraper {
   }
 
   async close() {
+    // Close the browser when done
     if (this.browser) {
       await this.browser.close();
     }
   }
 
   async delay(time) {
+    // Wait for a specified amount of time
     return new Promise((resolve) => setTimeout(resolve, time));
   }
 
-  async scrapeWebsite(websiteModule, url, delayTime, retries = 3) {
+  async fetchRobotsTxt(url) {
+    // Check if we've already fetched the robots.txt for this URL
+    if (!this.robotsTxtCache[url]) {
+      // Fetch the robots.txt
+      const robotsTxtUrl = new URL('/robots.txt', url).toString();
+      const response = await axios.get(robotsTxtUrl);
+
+      // Parse and cache the robots.txt
+      const robotsTxt = RobotsParser(robotsTxtUrl, response.data);
+      this.robotsTxtCache[url] = robotsTxt;
+    }
+
+    return this.robotsTxtCache[url];
+  }
+
+  async scrapeWebsite(websiteModule, url, delayTime = 1000, retries = 3) {
     let page = this.pages[url];
 
     while (retries) {
       try {
+        // Check robots.txt
+        const robotsTxt = await this.fetchRobotsTxt(url);
+        if (!robotsTxt.isAllowed(url, new UserAgent().toString())) {
+          console.error(`Scraping disallowed by robots.txt at ${url}`);
+          return null;
+        }
+
+        // Open the page and scrape data
         if (!page) {
           page = await this.browser.newPage();
           await page.setUserAgent(new UserAgent().toString());
           this.pages[url] = page;
           await page.goto(url, { waitUntil: 'networkidle2' });
+
           await this.delay(delayTime);
         }
 
@@ -41,7 +72,7 @@ class GenericScraper {
       } catch (err) {
         console.error(`Error scraping ${url}: ${err}`);
         retries--;
-        delayTime *= 2; // double the delay time
+        delayTime *= 2;
         console.log(`Retrying ${url}. Retries left: ${retries}`);
       }
     }
